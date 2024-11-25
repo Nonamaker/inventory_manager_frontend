aecbb898338314c97ade9075bdb0c5e87c249ebf
Elijah Nonamaker <nonamaker.e@gmail.com>
2024-11-25 14:13:04 +0000

    FontAwesome; registration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { useState, useContext, useEffect } from 'react';
 
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Stack from 'react-bootstrap/Stack';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router";
@@ -13,11 +22,174 @@ import { BrowserRouter, Routes, Route, useNavigate } from "react-router";
 import { authContext } from './contexts';
 import { PrivateRoute } from './PrivateRoute.js';
 
+export function AppNavbar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary mb-5">
+      <Container>
+        <Navbar.Brand href="/">Inventory Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/characters">Characters</Nav.Link>
+            <NavDropdown title="Profile" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/logout">
+                 Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+
+export function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordCheck, setShowPasswordCheck] = useState(false);
+  const [errors, setErrors] = useState([]);
+
+  const navigate = useNavigate();
+
+  const attemptRegister = () => {
+
+    if (passwordCheck !== password) {
+      setErrors(["Passwords do not match."]);
+      return;
+    }
+
+    fetch(
+      'http://192.168.1.10/register',
+      {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      }
+    )
+    .then(async (response) => {
+      if (response.status === 200) {
+          navigate("/login");
+      } else if (response.status === 400) {
+        const data = await response.json();
+        let errorMessages = [];
+        for (var prop in data.errors) {
+          errorMessages.push(...data.errors[prop]);
+        }
+        setErrors(errorMessages);
+      }
+    });
+  }
+
+  const errorList = errors.map((error, index) => {
+    return (
+      <Alert
+        key={index}
+        variant="warning"
+      >
+        {error}
+      </Alert>
+    )
+  });
+
+  const ErrorSection = () => {
+    if (errors.length > 0) { 
+      return (        
+        <div md={{ span: 4, offset: 4}}>
+          {errorList}
+        </div>
+      ) 
+    }
+    return null;
+  }
+
+  return (
+    <>
+      <Container fluid>
+        <Stack gap={3} className="col-xxl-2 offset-xxl-5 col-md-4 offset-md-4">
+          <div className="text-center">
+            <h3>Register</h3>
+          </div>
+          <div>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <InputGroup>
+              <Form.Control
+                type={showPassword ? "text" : "password"}
+                name="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <InputGroup.Text
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+              >
+                <FontAwesomeIcon icon={faEye} />
+              </InputGroup.Text>
+            </InputGroup>
+          </div>
+          <div>
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <InputGroup>
+              <Form.Control
+                type={showPasswordCheck ? "text" : "password"}
+                name="passwordCheck"
+                onChange={(e) => {
+                  setPasswordCheck(e.target.value);
+                }}
+              />
+              <InputGroup.Text
+                onClick={() => {
+                  setShowPasswordCheck(!showPasswordCheck);
+                }}
+              >
+                <FontAwesomeIcon icon={faEye} />
+              </InputGroup.Text>
+            </InputGroup>
+          </div>
+          <div className="d-grid gap-2">
+            <Button
+              onClick={() => {
+                attemptRegister();
+              }}
+            >Register</Button>
+            <Form.Text className="text-muted">
+              Already have an account? <a href="/login">Login</a>
+            </Form.Text>
+          </div>
+          <ErrorSection />  
+        </Stack>
+      </Container>
+    </>
+  )
+}
 
 export function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const context = useContext(authContext);
   const navigate = useNavigate();
@@ -50,13 +222,11 @@ export function Login() {
   return (
     <>
       <Container fluid>
-        <Row>
-          <Col className="text-center">
+        <Stack gap={3} className="col-xxl-2 offset-xxl-5 col-md-4 offset-md-4">
+          <div className="text-center">
             <h3>Login</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
+          </div>
+          <div>
             <Form.Label>Email Address</Form.Label>
             <Form.Control
               type="email"
@@ -65,29 +235,37 @@ export function Login() {
                 setEmail(e.target.value);
               }}
             />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
+          </div>
+          <div>
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <InputGroup>
+              <Form.Control
+                type={showPassword ? "text" : "password"}
+                name="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <InputGroup.Text
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+              >
+                <FontAwesomeIcon icon={faEye} />
+              </InputGroup.Text>
+            </InputGroup>
+          </div>
+          <div className="d-grid gap-2">
             <Button
               onClick={() => {
                 attemptLogin();
               }}
             >Login</Button>
-          </Col>
-        </Row>
+            <Form.Text className="text-muted">
+              Don't have an account? <a href="/register">Register</a>
+            </Form.Text>
+          </div>
+        </Stack>
       </Container>
     </>
   )
@@ -101,6 +279,7 @@ function Logout() {
   useEffect(() => {
     context.setBearerToken("");
     context.setAuthenticated("");  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   navigate("/login");
@@ -128,6 +307,7 @@ export function App() {
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter> ) : null
   )
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { AuthContextProvider } from './contexts';
-import { App } from './App';
+import { App, AppNavbar } from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -10,7 +10,10 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  <>
+  <AppNavbar />
   <AuthContextProvider>
     <App />
   </AuthContextProvider>
+  </>
 );
